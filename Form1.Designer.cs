7a4732a215be877307771b9ca018ffd11fcd6666
Hanieh Navab <107445808+Hani-navab@users.noreply.github.com>
2022-06-14 12:25:49 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ColorCheckDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sdaColor = new System.Data.SqlClient.SqlDataAdapter();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.dsColor1 = new ColorCheckDemo.dsColor();
+            this.flpColor = new System.Windows.Forms.FlowLayoutPanel();
+            this.dbgProduct = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dsColor1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbgProduct)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = "SELECT DISTINCT ISNULL(Color, \'[No Color]\') AS \'Color\'\r\nFROM         Production.P" +
+                "roduct";
+            this.sqlSelectCommand1.Connection = this.sqlConnection1;
+            // 
+            // sdaColor
+            // 
+            this.sdaColor.SelectCommand = this.sqlSelectCommand1;
+            this.sdaColor.TableMappings.AddRange(new System.Data.Common.DataTableMapping[] {
+            new System.Data.Common.DataTableMapping("Table", "Table", new System.Data.Common.DataColumnMapping[] {
+                        new System.Data.Common.DataColumnMapping("Color", "Color")})});
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=.;Initial Catalog=AdventureWorks;Persist Security Info=True;User ID=s" +
+                "a;Password=sql";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // dsColor1
+            // 
+            this.dsColor1.DataSetName = "dsColor";
+            this.dsColor1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // flpColor
+            // 
+            this.flpColor.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flpColor.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flpColor.Location = new System.Drawing.Point(0, 0);
+            this.flpColor.Name = "flpColor";
+            this.flpColor.Size = new System.Drawing.Size(445, 104);
+            this.flpColor.TabIndex = 0;
+            // 
+            // dbgProduct
+            // 
+            this.dbgProduct.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbgProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbgProduct.Location = new System.Drawing.Point(0, 104);
+            this.dbgProduct.Name = "dbgProduct";
+            this.dbgProduct.Size = new System.Drawing.Size(445, 316);
+            this.dbgProduct.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 420);
+            this.Controls.Add(this.dbgProduct);
+            this.Controls.Add(this.flpColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dsColor1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbgProduct)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlDataAdapter sdaColor;
+        private dsColor dsColor1;
+        private System.Windows.Forms.FlowLayoutPanel flpColor;
+        private System.Windows.Forms.DataGridView dbgProduct;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ColorCheckDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sdaColor.Fill(dsColor1);
+            foreach (DataRow r in dsColor1.Tables[0].Rows)
+            {
+                CheckBox chk = new CheckBox();
+                chk.CheckedChanged += new EventHandler(chk_CheckedChanged);
+                chk.Text = r["Color"].ToString();
+                flpColor.Controls.Add(chk);
+            }
+        }
+
+        void chk_CheckedChanged(object sender, EventArgs e)
+        {
+            string ColorCollection = "";
+            foreach (Control c in flpColor.Controls)
+            {
+                if (((CheckBox)c).Checked)
+                { 
+                    if(ColorCollection.Length==0)
+                        ColorCollection=string.Format("'{0}'",c.Text);
+                    else
+                        ColorCollection+=string.Format(",'{0}'",c.Text);
+                }
+            }
+            if (ColorCollection.Length == 0)
+                dbgProduct.DataSource = null;
+            else
+            {
+                SqlCommand cmd = new SqlCommand(string.Format("SELECT ProductID,Name,Color FROM Production.Product WHERE ISNULL(Color,'[No Color]') IN({0})",ColorCollection),sqlConnection1);
+                DataTable tbl = new DataTable();
+                cmd.Connection.Open();
+                tbl.Load(cmd.ExecuteReader());
+                cmd.Connection.Close();
+                dbgProduct.DataSource = tbl;
+            }
+        }
+    }
+}
